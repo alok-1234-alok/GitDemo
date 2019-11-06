173902ec6616e4eef7038dcc9237508e95368ea3
Alok <alok0053@gmail.com>
2019-11-06 17:06:16 +0530

    First commit

---
+++ src/com/nt/Test.java
@@ -0,0 +1,16 @@
+package com.nt;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubg
+		String hungry="yes";
+		if(hungry=="yes") {
+			System.out.println("eat something");
+		}
+		else {
+			System.out.println("do workout");
+		}
+	}
+
+}
